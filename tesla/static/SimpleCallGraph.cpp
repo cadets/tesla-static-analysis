b3c719cd4744884c2a606b25bab48cfd0ca296ed
Bruce Collie <bsc28@cam.ac.uk>
2017-01-26 16:05:58 +0000

    Implement construction of adjacency entries

--- tesla/static/SimpleCallGraph.cpp
+++ tesla/static/SimpleCallGraph.cpp
@@ -1,1 +1,24 @@
 #include "SimpleCallGraph.h"
+
+#include <llvm/IR/Instructions.h>
+
+SimpleCallGraph::SimpleCallGraph(Module &M) {
+  for(auto &F : M) {
+    Adjacency[&F] = getAdjacency(&F);
+  }
+}
+
+vector<Function *> SimpleCallGraph::getAdjacency(Function *f) {
+  vector<Function *> called{};
+
+  for(auto &BB : *f) {
+    for(auto &I : BB) {
+      if(isa<CallInst>(I)) {
+        auto &call = cast<CallInst>(I);
+        called.push_back(call.getCalledFunction());
+      }
+    }
+  }
+
+  return called;
+}
--- tesla/static/SimpleCallGraph.h
+++ tesla/static/SimpleCallGraph.h
@@ -1,4 +1,26 @@
 #ifndef SIMPLE_CALL_GRAPH_H
 #define SIMPLE_CALL_GRAPH_H
 
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Module.h>
+
+#include <map>
+#include <vector>
+
+using std::map;
+using std::vector;
+using namespace llvm;
+
+struct SimpleCallGraph {
+  
+  /**
+   * Construct a call graph from the given module.
+   */
+  SimpleCallGraph(Module &M);
+
+private:
+  map<Function *, vector<Function *>> Adjacency;
+  static vector<Function *> getAdjacency(Function *f);
+};
+
 #endif
