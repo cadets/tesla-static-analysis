7d23b3badd24a95fef4ef0bc5fe5cc8d26aab65f
Bruce Collie <bsc28@cam.ac.uk>
2017-01-26 17:32:01 +0000

    Add some examples

---
+++ experiments/locks/rel_before_indirect.c
@@ -0,0 +1,23 @@
+#include "lock.h"
+
+/**
+ * Lock function implementations
+ * 
+ * In this program, the lock functions are stubs that do not actually perform
+ * any locking. Instead, they mock out the correct behaviour of a lock.
+ */
+
+lock_t lock;
+
+void acq() {
+  while(!lock_acquire(&lock)) {}
+}
+
+void rel() {
+  lock_release(&lock);
+}
+
+void do_work(void) {
+  rel();
+  acq();
+}
