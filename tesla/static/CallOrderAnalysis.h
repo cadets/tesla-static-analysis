2d7dbcb16982ea5020ba65cba6d876a487a4c7a4
Bruce Collie <bsc28@cam.ac.uk>
2017-01-26 16:55:23 +0000

    Add call graph to call order analysis

--- tesla/static/CallOrderAnalysis.h
+++ tesla/static/CallOrderAnalysis.h
@@ -2,18 +2,20 @@
 #define CALL_ORDER_ANALYSIS_H
 
 #include "Analysis.h"
+#include "SimpleCallGraph.h"
 
 #include <llvm/IR/Function.h>
 #include <llvm/IR/Module.h>
 
 struct CallOrderAnalysis : public Analysis {
   CallOrderAnalysis(Module &M, Function &F) : 
-    Analysis(M), Bound(F) {}
+    Analysis(M), Bound(F), CG(M) {}
 
   bool run() override;
   std::string AnalysisName() const override { return "CallOrderAnalysis"; }
 private:
   Function &Bound;
+  SimpleCallGraph CG;
 };
 
 #endif
