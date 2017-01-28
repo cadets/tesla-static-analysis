3f60dde8a737e3e30b414470e8da9454f25b3ec3
Bruce Collie <bsc28@cam.ac.uk>
2017-01-28 16:46:42 +0000

    Implement missing call analysis

--- tesla/static/MissingCallAnalysis.h
+++ tesla/static/MissingCallAnalysis.h
@@ -2,16 +2,18 @@
 #define MISSING_CALL_ANALYSIS_H
 
 #include "Analysis.h"
+#include "SimpleCallGraph.h"
 
 using namespace llvm;
 
 struct MissingCallAnalysis : public Analysis {
   MissingCallAnalysis(Module &M, Function &F) :
-    Analysis(M), Bound(F) {}
+    Analysis(M), CG(M), Bound(F) {}
 
   std::string AnalysisName() const override { return "MissingCallAnalysis"; }
   bool run() override;
 private:
+  SimpleCallGraph CG;
   Function &Bound;
 };
 
