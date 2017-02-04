b942a4c81b10069d454fa2042f517bbd060b4000
Bruce Collie <bsc28@cam.ac.uk>
2017-02-04 12:31:23 +0000

    Fix acq rel when it's running on another thing

--- experiments/call_seq/main.c
+++ experiments/call_seq/main.c
@@ -1,3 +1,12 @@
+#include <tesla-macros.h>
+#include <tesla.h>
+
+void f(void) {}
+
+void g(void) {}
+
 int main(void) {
+  TESLA_WITHIN(main, eventually(TSEQUENCE(call(f), call(g))));
+
   return 0;
 }
