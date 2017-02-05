ad0425159be28ee2ae8aee5b9f902fd615342fa2
Bruce Collie <bsc28@cam.ac.uk>
2017-02-05 16:17:12 +0000

    Run new pass

--- tesla/static/CallSequencePass.cpp
+++ tesla/static/CallSequencePass.cpp
@@ -1,9 +1,20 @@
 #include "CallSequencePass.h"
 
+#include <llvm/Support/raw_ostream.h>
+
 namespace tesla {
     
-unique_ptr<Manifest> CallSequencePass::run(Manifest &Ma, llvm::Module &Mo) {
-  return nullptr;
+unique_ptr<Manifest> CallSequencePass::run(Manifest &Man, llvm::Module &Mo) {
+  auto File = new ManifestFile();
+
+  copyDefinitions(Man, File);
+  for (auto root : Man.RootAutomata()) {
+    copyUsage(root, File);
+  }
+
+  auto unique = unique_ptr<ManifestFile>(File);
+  return unique_ptr<Manifest>(Manifest::construct(
+      llvm::errs(), Automaton::Deterministic, std::move(unique)));
 };
 
 }
