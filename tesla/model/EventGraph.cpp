b347404358e28ee038e8e0cf4452bb9f1bdadac9
Bruce Collie <bsc28@cam.ac.uk>
2017-02-13 17:44:54 +0000

    Construct graph from basic block

--- tesla/model/EventGraph.cpp
+++ tesla/model/EventGraph.cpp
@@ -1,1 +1,25 @@
 #include "EventGraph.h"
+
+EventGraph::EventGraph(Function *b) {
+}
+
+EventGraph::EventGraph(BasicBlock *bb) {
+  RootNode = nullptr;
+
+  auto tail = RootNode;
+  for(auto &I : *bb) {
+    if(auto call = dyn_cast<CallInst>(&I)) {
+      auto node = new CallNode(call);
+
+      if(!tail) {
+        RootNode = node;
+        tail = node;
+      } else {
+        tail->addNeighbour(node);
+        tail = node;
+      }
+    }
+  }
+
+  ExitNode = tail;
+}
--- tesla/model/EventGraph.h
+++ tesla/model/EventGraph.h
@@ -3,36 +3,53 @@
 
 #include <llvm/IR/Function.h>
 #include <llvm/IR/Instructions.h>
+#include <llvm/IR/Module.h>
 #include <llvm/IR/Value.h>
 
+#include <map>
 #include <vector>
 
+using std::map;
+using std::string;
 using std::vector;
 using namespace llvm;
 
 struct EventNode {
-  EventNode(vector<EventNode *> ns) :
-    neighbours(ns) {}
+  EventNode() : neighbours({}) {}
 
   virtual Value *value() const = 0;
-  const vector<EventNode *> neighbours;
+  virtual string name() const = 0;
+  vector<EventNode *> neighbours;
+
+  void addNeighbour(EventNode *n) { neighbours.push_back(n); }
+
+  bool operator==(const EventNode &other) const {
+    return value() == other.value();
+  }
 };
 
 struct CallNode : public EventNode {
-  CallNode(vector<EventNode *> ns, CallInst *c) :
-    EventNode(ns), call(c) {}
+  CallNode(CallInst *c) :
+    EventNode(), call(c) {}
 
   Value *value() const override { return call; }
+  string name() const override { return call->getCalledFunction()->getName().str(); }
 private:
   CallInst *call;
 };
 
 struct EventGraph {
-  EventGraph(Function *b) :
-    Bound(b) {}
+  EventGraph(Function *b);
+  EventGraph(BasicBlock *bb);
 
+  const EventNode& Root() const { return *RootNode; }
+  const EventNode& Exit() const { return *ExitNode; }
+  bool Empty() const { return !RootNode; }
 private:
-  Function *Bound;
+  map<Function *, EventGraph *> funcCache;
+  map<BasicBlock *, EventGraph *> bbCache;
+  EventNode *RootNode;
+  EventNode *ExitNode;
 };
 
 #endif
