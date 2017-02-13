0280011793f7d514eaf7c05737cf192cb9a3abc4
Bruce Collie <bsc28@cam.ac.uk>
2017-02-13 17:48:57 +0000

    Load module from cli arg

--- tesla/model/model.cpp
+++ tesla/model/model.cpp
@@ -1,3 +1,26 @@
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/SourceMgr.h>
+
+using namespace llvm;
+
+static cl::opt<std::string>
+BitcodeFilename(cl::Positional, cl::desc("bitcode"), cl::Required);
+
 int main(int argc, char **argv) {
+  SMDiagnostic Err;
+  LLVMContext &Context = getGlobalContext();
+
+  cl::ParseCommandLineOptions(argc, argv, "TESLA IR Model Builder\n");
+
+  std::unique_ptr<Module> Mod(ParseIRFile(BitcodeFilename, Err, Context));
+  if(Mod.get() == nullptr) {
+    Err.print(argv[0], errs());
+    return 1;
+  }
+  
   return 0;
 }
