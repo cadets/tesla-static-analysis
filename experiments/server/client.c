6df8475decdf28a7e0fb7ddfcd12d0807b4c5c3b
Bruce Collie <bsc28@cam.ac.uk>
2017-03-08 17:46:44 +0000

    Implement minimal client that can connect to a server

--- experiments/server/client.c
+++ experiments/server/client.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 
 void usage(char *me) {
   fprintf(stderr, "usage: %s server port\n", me);
@@ -25,6 +26,21 @@ int main(int argc, char **argv) {
   if(port <= 0) {
     fprintf(stderr, "Invalid port number: %s (%s)\n", argv[2], strerror(errno));
     return 3;
+  }
+
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  struct sockaddr_in serv_addr;
+  serv_addr.sin_family = AF_INET;
+  bcopy(
+      (char *)server->h_addr,
+      (char *)&serv_addr.sin_addr.s_addr,
+      server->h_length
+  );
+  serv_addr.sin_port = htons(port);
+
+  if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    fprintf(stderr, "Error connecting (%s)\n", strerror(errno));
   }
 
   return 0;
