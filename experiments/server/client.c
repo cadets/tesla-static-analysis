b60ee621ed94c39373a2fcee123c177b595dd2a7
Bruce Collie <bsc28@cam.ac.uk>
2017-03-08 17:55:20 +0000

    Split out some stuff

--- experiments/server/client.c
+++ experiments/server/client.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <errno.h>
 #include <netinet/in.h>
 #include <netdb.h>
@@ -8,6 +9,37 @@
 
 void usage(char *me) {
   fprintf(stderr, "usage: %s server port\n", me);
+}
+
+struct sockaddr_in addr_from_args(struct hostent *server, long port) {
+  struct sockaddr_in serv_addr;
+
+  serv_addr.sin_family = AF_INET;
+  bcopy(
+      (char *)server->h_addr,
+      (char *)&serv_addr.sin_addr.s_addr,
+      server->h_length
+  );
+  serv_addr.sin_port = htons(port);
+
+  return serv_addr;
+}
+
+int connect_to(struct hostent *server, long port) {
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sockfd < 0) {
+    fprintf(stderr, "Error creating socket (%s)\n", strerror(errno));
+  }
+
+  struct sockaddr_in serv_addr = addr_from_args(server, port);
+  int conn = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+  if(conn < 0) {
+    fprintf(stderr, "Error connecting (%s)\n", strerror(errno));
+    return conn;
+  }
+
+  return sockfd;
 }
 
 int main(int argc, char **argv) {
@@ -28,19 +60,9 @@ int main(int argc, char **argv) {
     return 3;
   }
 
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-  struct sockaddr_in serv_addr;
-  serv_addr.sin_family = AF_INET;
-  bcopy(
-      (char *)server->h_addr,
-      (char *)&serv_addr.sin_addr.s_addr,
-      server->h_length
-  );
-  serv_addr.sin_port = htons(port);
-
-  if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    fprintf(stderr, "Error connecting (%s)\n", strerror(errno));
+  int sockfd = connect_to(server, port);
+  if(sockfd < 0) {
+    return 4;
   }
 
   return 0;
