820a1df92287cc64912c9f2837baacd99917fbb1
Bruce Collie <bsc28@cam.ac.uk>
2017-03-09 15:54:35 +0000

    Have client and server talk to each other properly

--- experiments/server/server_handler.c
+++ experiments/server/server_handler.c
@@ -7,12 +7,14 @@
 struct server_state {
   int fd;
   bool active;
+  uint16_t next;
 };
 
 struct server_state *init_state(int fd) {
   struct server_state *state = malloc(sizeof(*state));
   state->fd = fd;
   state->active = true;
+  state->next = 0;
   return state;
 }
 
@@ -26,16 +28,50 @@ void handle_connection(int fd) {
 }
 
 void handle_request(uint16_t n, void *state) {
+  struct server_state *sst = (struct server_state *)state;
+
+  printf("Received request to send %d packets\n", n);
+
+  struct packet permit = {
+    .kind = PK_PERMIT,
+    .seq_no = n,
+    .data = { 0 }
+  };
+  send_packet(sst->fd, permit);
 }
 
 void handle_permit(uint16_t n, void *state) {
+  struct server_state *sst = (struct server_state *)state;
+  sst->active = false;
 }
 
 void handle_data(uint8_t *data, uint16_t sn, void *state) {
+  struct server_state *sst = (struct server_state *)state;
+
+  if(sn != sst->next) {
+    struct packet done = {
+      .kind = PK_DONE,
+      .seq_no = 0,
+      .data = { 0 }
+    };
+    send_packet(sst->fd, done);
+    return;
+  }
+
+  struct packet ack = {
+    .kind = PK_ACK,
+    .seq_no = sst->next++,
+    .data = { 0 }
+  };
+  send_packet(sst->fd, ack);
 }
 
 void handle_ack(uint16_t sn, void *state) {
+  struct server_state *sst = (struct server_state *)state;
+  sst->active = false;
 }
 
 void handle_done(void *state) {
+  struct server_state *sst = (struct server_state *)state;
+  sst->active = false;
 }
