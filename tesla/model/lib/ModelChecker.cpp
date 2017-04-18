4de005e7d91f10638e11addfeaf346e08c9b25d0
Bruce Collie <bsc28@cam.ac.uk>
2017-04-18 10:51:05 +0000

    Finish FSM checking algorithm

--- tesla/model/lib/ModelChecker.cpp
+++ tesla/model/lib/ModelChecker.cpp
@@ -29,7 +29,7 @@ bool ModelChecker::IsUsageSafe(const tesla::Usage *use) {
   auto done = false;
   auto&& mut = std::mutex{};
 
-  auto n_cores = std::thread::hardware_concurrency();
+  auto n_cores = 1;//std::thread::hardware_concurrency();
   auto&& trace_gen = FiniteTraces{Graph};
 
   auto fsm = Gen.FSM();
@@ -109,20 +109,15 @@ set<const tesla::Usage *> ModelChecker::SafeUsages() {
   return safeUses;
 }
 
-// Need to keep track of a return constraint as we're going through the FSM.
-// What if each work item is given a return constraint when it's built (i.e. the
-// one corresponding to the edge that led to it)? This third member needs to be
-// passed through if the next event doesn't supply a new constraint.
-
 bool ModelChecker::CheckAgainstFSM(const FiniteTraces::Trace &tr, const FiniteStateMachine<Expression *> fsm, bool cycle)
 {
   struct work_item {
-    work_item(std::shared_ptr<::State> s, size_t b, std::shared_ptr<Expression> e) :
+    work_item(std::shared_ptr<::State> s, size_t b, std::shared_ptr<BoolValue> e) :
       state(s), begin(b), expr(e) {}
 
     std::shared_ptr<::State> state;
     size_t begin;
-    std::shared_ptr<Expression> expr;
+    std::shared_ptr<BoolValue> expr;
 
     bool operator<(const work_item& o) const {
       return std::tie(state, begin, expr) < std::tie(o.state, o.begin, o.expr);
@@ -135,7 +130,7 @@ bool ModelChecker::CheckAgainstFSM(const FiniteTraces::Trace &tr, const FiniteSt
   work_queue.push(work_item(start_state, 0, nullptr));
 
   auto visited = std::set<work_item>{};
-  
+
   while(!work_queue.empty()) {
     auto next = work_queue.top();
     work_queue.pop();
@@ -155,12 +150,32 @@ bool ModelChecker::CheckAgainstFSM(const FiniteTraces::Trace &tr, const FiniteSt
     }
 
     for(const auto& edge : fsm.Edges(next.state)) {
-      auto accepts = edge.Accepts<Event *>(tr[next.begin], [=](auto ev, auto ex) { 
-        return CheckState(*ex, ev, true); 
+      auto new_c = next.expr;
+
+      auto accepts = edge.Accepts<Event *>(tr[next.begin], [=,&new_c](auto ev, auto ex) { 
+        auto bigram_match = true;
+
+        if(auto exe = dyn_cast<ExitEvent>(ev)) {
+          if(exe->Call && hasReturnConstraint(ex)) {
+            auto val = static_cast<bool>(getReturnConstraint(ex));
+            auto&& constraint = std::make_shared<BoolValue>(exe->Call, val);
+
+            bigram_match = bigram_match && ConstraintsOccur(BBGraph, {*constraint});
+
+            if(next.expr) {
+              auto&& bigram = std::make_pair(*next.expr, *constraint);
+              bigram_match = bigram_match && AssertionPairs.find(bigram) != AssertionPairs.end();
+            }
+
+            new_c = constraint;
+          }
+        }
+
+        return CheckState(*ex, ev, true) && bigram_match;
       });
 
       if(accepts) {
-        work_queue.push(work_item(edge.End(), next.begin+1, next.expr));
+        work_queue.push(work_item(edge.End(), next.begin+1, new_c));
       }
     }
   }
