98585cd2e7ffcc1afba3cdb1de4046082ada9713
Bruce Collie <bsc28@cam.ac.uk>
2017-04-24 16:33:38 +0000

    get bound function in z3 checker

--- tesla/trace/include/z3_checker.h
+++ tesla/trace/include/z3_checker.h
@@ -1,20 +1,26 @@
 #ifndef Z3_CHECKER_H
 #define Z3_CHECKER_H
 
+#include <llvm/IR/Function.h>
 #include <llvm/IR/Module.h>
 
 #include "Manifest.h"
 #include "tesla.pb.h"
 
+using namespace llvm;
+
 class Z3Checker {
 public:
-  Z3Checker(tesla::Manifest &man, llvm::Module &mo) :
+  Z3Checker(tesla::Manifest &man, Module &mo) :
     manifest_(man), module_(mo) {}
 
   bool check_usage(const tesla::Usage *use);
 private:
   tesla::Manifest& manifest_;
-  llvm::Module& module_;
+  Module& module_;
+
+  bool has_checkable_bounds(const tesla::Usage *use) const;
+  Function *bound_function(const tesla::Usage *use) const;
 };
 
 #endif
--- tesla/trace/lib/z3_checker.cpp
+++ tesla/trace/lib/z3_checker.cpp
@@ -1,10 +1,47 @@
+#include <llvm/Support/raw_ostream.h>
+
+#include "FSMBuilder.h"
 #include "z3_checker.h"
 
 bool Z3Checker::check_usage(const tesla::Usage *use)
 {
+  if(!has_checkable_bounds(use)) {
+    return false;
+  }
+
   auto automaton = manifest_.FindAutomaton(use->identifier());
   auto expr = automaton->getAssertion().expression();
 
-  //auto fsm = ModelGenerator(expr, Manifest).FSM().Deterministic().Relabeled();
+  auto fsm = FSMBuilder(expr, &manifest_).FSM().Deterministic().Relabeled();
   return false;
+}
+
+bool Z3Checker::has_checkable_bounds(const tesla::Usage *use) const
+{
+  const auto& begin = use->beginning();
+  const auto& end = use->end();
+
+  if(begin.type() != Expression_Type_FUNCTION ||
+     end.type() != Expression_Type_FUNCTION) {
+    return false;
+  }
+
+  const auto& begin_event = begin.function();
+  const auto& end_event = end.function();
+  
+  if(begin_event.direction() != FunctionEvent_Direction_Entry ||
+     end_event.direction() != FunctionEvent_Direction_Exit) {
+    return false;
+  }
+
+  return begin_event.function().name() == end_event.function().name();
+}
+
+Function *Z3Checker::bound_function(const tesla::Usage *use) const
+{
+  if(!has_checkable_bounds(use)) {
+    return nullptr;
+  }
+
+  return module_.getFunction(use->end().function().function().name());
 }
