9e02e72055de7e2fc9449bd9e8aa34ce3da0a079
Bruce Collie <bsc28@cam.ac.uk>
2017-08-30 11:33:49 +0100

    Load manifest file

--- tesla/instrumenter/InstrumentPass.cpp
+++ tesla/instrumenter/InstrumentPass.cpp
@@ -1,3 +1,6 @@
+#include "Debug.h"
+#include "Manifest.h"
+
 #include <llvm/Pass.h>
 #include <llvm/IR/Function.h>
 #include <llvm/Support/raw_ostream.h>
@@ -11,6 +14,11 @@ struct InstrumentPass : public ModulePass {
   InstrumentPass() : ModulePass(ID) {}
 
   bool runOnModule(Module& M) override {
+    std::unique_ptr<tesla::Manifest> Manifest(tesla::Manifest::load(llvm::errs()));
+    if (!Manifest) {
+      tesla::panic("unable to load TESLA manifest");
+    }
+
     return false;
   }
 };
